f78d173770b1ead1f58914b00e53cb659effd14c
michaelfm1211 <michaelm1211@protonmail.com>
2020-07-29 21:18:48 -0500

    Fix #1 & Security Vurnabilities

--- src/index.js
+++ src/index.js
@@ -17,14 +17,16 @@ import p5 from "p5";
 import Ball from "./Ball.js";
 import Paddle from "./Paddle.js";
 
-let sketch = function(p) {
+let sketch = function (p) {
 	let paddle;
 	let score;
 	let shorterDim;
 	let balls;
 	let dead;
 
-	p.setup = function() {
+	let ball_creation_count;
+
+	p.setup = function () {
 		p.createCanvas(p.windowWidth, p.windowHeight);
 		p.noCursor();
 		paddle = new Paddle(p);
@@ -32,26 +34,27 @@ let sketch = function(p) {
 		dead = false;
 		shorterDim = p.height > p.width ? p.height : p.width;
 		balls = [];
-
-		setInterval(() => balls.push(new Ball(p, p.createVector(this.width/2, this.height/2), p.createVector(p.random(-1.5, -1), p.random(-1.5, 1.5)), {
-			paddle: paddle,
-			score: amount => score += amount,
-			die: () => dead = true
-		})), 1000);
+		ball_creation_count = 0;
 	};
 
-	p.draw = function() {
+	p.draw = function () {
 		p.background(0, dead ? 200 : 255);
 
-		if(dead){
+		if (dead) {
 			p.strokeWeight(1);
 			p.fill(255, 0, 0);
 			p.stroke(255, 0, 0);
-			p.textSize(shorterDim/20);
+			p.textSize(shorterDim / 20);
 			p.textAlign(p.CENTER, p.CENTER);
-			p.text("Game Over", 0, 0, p.width, p.height/2);
-			p.textSize(shorterDim/40);
-			p.text("Press R to Restart", 0, 0, p.width, p.height/2 + ((shorterDim/20) * 1.5));
+			p.text("Game Over", 0, 0, p.width, p.height / 2);
+			p.textSize(shorterDim / 40);
+			p.text(
+				"Press R to Restart",
+				0,
+				0,
+				p.width,
+				p.height / 2 + (shorterDim / 20) * 1.5
+			);
 			p.cursor(p.ARROW);
 
 			p.noLoop();
@@ -61,24 +64,42 @@ let sketch = function(p) {
 		p.strokeWeight(1);
 		p.fill(255, 100);
 		p.stroke(255, 100);
-		p.textSize(shorterDim/40);
+		p.textSize(shorterDim / 40);
 		p.textAlign(p.CENTER, p.CENTER);
-		p.text(`Score: ${score}`, 0, 0, p.width, shorterDim/14);
+		p.text(`Score: ${score}`, 0, 0, p.width, shorterDim / 14);
 
-		balls.forEach(ball => {
+		balls.forEach((ball) => {
 			ball.show();
 			ball.update();
 		});
 
 		paddle.show();
+
+		if (ball_creation_count >= p.frameRate()) {
+			balls.push(
+				new Ball(
+					p,
+					p.createVector(p.width / 2, p.height / 2),
+					p.createVector(p.random(-1.5, -1), p.random(-1.5, 1.5)),
+					{
+						paddle: paddle,
+						score: (amount) => (score += amount),
+						die: () => (dead = true),
+					}
+				)
+			);
+			ball_creation_count = 0;
+		} else {
+			ball_creation_count++;
+		}
 	};
 
-	p.mouseMoved = function() {
+	p.mouseMoved = function () {
 		paddle.update();
 	};
 
-	p.keyPressed = function() {
-		if(p.keyCode == 82 && dead == true){
+	p.keyPressed = function () {
+		if (p.keyCode == 82 && dead == true) {
 			p.background(0);
 			balls = [];
 			dead = false;
